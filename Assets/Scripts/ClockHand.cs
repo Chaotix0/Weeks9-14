57e126456d0186e4abe2d03037bb906e40ce52d9
framptoj <framptoj@OA-S141-22>
2025-03-11 18:46:29 -0400

    clock

---
+++ Assets/Scripts/ClockHand.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ClockHand : MonoBehaviour
+{
+
+    public UnityEvent TimerHasFinshed;
+    public float timerLength = 2;
+    public float t;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, -15 * Time.deltaTime);
+        t += Time.deltaTime;
+        if (t > timerLength)
+        {
+            t = 0;
+            TimerHasFinshed.Invoke();
+        }
+    }
+
+    public void kookoo()
+    {
+        Debug.Log("Button pushed");
+    }
+}
