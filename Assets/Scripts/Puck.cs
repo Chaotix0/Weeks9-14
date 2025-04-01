c89f9813df2e16277c8cc999a016123ce52c3e1d
framptoj <framptoj@OA-S141-22>
2025-04-01 19:00:29 -0400

    puck spawns and disappears after the size is 0

--- Assets/Scripts/Puck.cs
+++ Assets/Scripts/Puck.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Puck : MonoBehaviour
@@ -10,19 +11,34 @@ public class Puck : MonoBehaviour
     public float maxYPosition = 0; // Right boundary
     public float minYPosition = -2; // Left boundary
     public AnimationCurve size; //puck size
-    public AnimationCurve XValue;
-    public AnimationCurve YValue;
+    float t;
+    public float XValue;
+    public float YValue;
     public int net = 0;
     public GameObject goalie;
+    public GameObject puck;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        t += Time.deltaTime;
+        XValue = Random.Range(minXPosition, maxXPosition); //random X value
+        YValue = Random.Range(minYPosition, maxYPosition); //random Y value
+        transform.localScale = Vector2.one * size.Evaluate(t); //scales down puck size
+        PuckSpawn(); //spawns puck eqach frame
+    }
+    void PuckSpawn()
+    {
+        if (transform.localScale.x <= 0)
+        {
+            Instantiate(puck);
+            Destroy(gameObject); // Detroys puck when done
+        }
 
     }
 }
