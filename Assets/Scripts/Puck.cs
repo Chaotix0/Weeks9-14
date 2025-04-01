0ca2eabd5717b3a83f02ed20800bf2064bba7873
framptoj <framptoj@OA-S143-10>
2025-04-01 15:24:54 -0400

    puck code

--- Assets/Scripts/Puck.cs
+++ Assets/Scripts/Puck.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Puck : MonoBehaviour
 {
+    public float speed = 7; // puck speed
+    public float maxXPosition = 2; // Right boundary
+    public float minXPosition = -2; // Left boundary
+    public float maxYPosition = 0; // Right boundary
+    public float minYPosition = -2; // Left boundary
+    public AnimationCurve size; //puck size
+    public AnimationCurve XValue;
+    public AnimationCurve YValue;
+    public int net = 0;
+    public GameObject goalie;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
