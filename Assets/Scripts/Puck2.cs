7906975f36ff880847a930533aa6ed062771f8f9
framptoj <framptoj@OA-S138-27>
2025-04-04 15:00:46 -0400

    made the Crazy puck and made the animation curve for it work

--- Assets/Scripts/Puck2.cs
+++ Assets/Scripts/Puck2.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Puck2 : MonoBehaviour
 {
+    public float maxXPosition = 2; // Right boundary
+    public float minXPosition = -2; // Left boundary
+    public float maxYPosition = 0; // Right boundary
+    public float minYPosition = -2; // Left boundary
+    public AnimationCurve size; //puck size
+    public AnimationCurve crazy; //CrazyPuck movement
+    float t;
+    public float XValue;
+    public float YValue;
+    public GameObject goalie;
+    public GameObject puck;
+    public GameObject score;
+    public UnityEvent SAVE; // adding to the score when save the puck
     // Start is called before the first frame update
     void Start()
     {
-        
+        //StartCoroutine(Save());
+        XValue = Random.Range(minXPosition, maxXPosition); //random X value
+        YValue = Random.Range(minYPosition, maxYPosition); //random Y value
+
+        transform.position = new Vector3(XValue, YValue); //random puck spots
+
+        SAVE = new UnityEvent();
+        SAVE.AddListener(score.GetComponent<Score>().save);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        t += Time.deltaTime; //makes sure the puck scales every frame evenly
+        transform.localScale = Vector2.one * size.Evaluate(t); //scales down puck size
+        transform.position = Vector3.Lerp(new Vector3(XValue - 1, YValue), new Vector3(XValue, YValue), crazy.Evaluate(t)); //Crazy puck movement
+        PuckEnd(); //spawns puck eqach frame
+    }
+
+    void PuckEnd()
+    {
+        if (transform.localScale.x <= 0.001f)
+        {
+            if (goalie.transform.position.x >= puck.transform.position.x - 1.2f && goalie.transform.position.x <= puck.transform.position.x + 1.2f)// checking hitboxes for goalie
+            {
+                Instantiate(puck); //makes duplicate puck
+                SAVE.Invoke();
+            }
+            Destroy(gameObject); // Detroys puck when done
+
+        }
     }
 }
