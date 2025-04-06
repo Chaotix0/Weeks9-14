8224ea8366518dd63db90816151075839fac8349
Joshua <127633954+Chaotix0@users.noreply.github.com>
2025-04-06 14:51:02 -0400

    made progress with the Crazy puck

--- Assets/Scripts/Puck.cs
+++ Assets/Scripts/Puck.cs
@@ -11,17 +11,18 @@ public class Puck : MonoBehaviour
     public float maxYPosition = 0; // Right boundary
     public float minYPosition = -2; // Left boundary
     public AnimationCurve size; //puck size
+    public int num = 0;
     float t;
     public float XValue;
     public float YValue;
     public GameObject goalie;
     public GameObject puck;
     public GameObject score;
+    public GameObject CrazyPuck;
     public UnityEvent SAVE; // adding to the score when save the puck
     // Start is called before the first frame update
     void Start()
     {
-        //StartCoroutine(Save());
         XValue = Random.Range(minXPosition, maxXPosition); //random X value
         YValue = Random.Range(minYPosition, maxYPosition); //random Y value
 
@@ -29,25 +30,40 @@ public class Puck : MonoBehaviour
         
         SAVE = new UnityEvent();
         SAVE.AddListener(score.GetComponent<Score>().save);
-
+        num = Random.Range(0, 11);
     }
 
     // Update is called once per frame
     void Update()
     {
-        t += Time.deltaTime; //makes sure the puck scales every frame evenly
-        transform.localScale = Vector2.one * size.Evaluate(t); //scales down puck size
-        PuckEnd(); //spawns puck eqach frame
+        if (num != 10) //call normal puck
+        {
+            t += Time.deltaTime; //makes sure the puck scales every frame evenly
+            transform.localScale = Vector2.one * size.Evaluate(t); //scales down puck size
+            PuckEnd(); //spawns puck eqach frame
+        }
+        if (num == 10) // call Crazy puck
+        {
+            StartCoroutine(CrazyPuck.GetComponent<Puck2>().Crazy()); //start CrazyPuck
+
+            t += Time.deltaTime; //makes sure the puck scales every frame evenly
+            transform.localScale = Vector2.one * size.Evaluate(t); //scales down puck size
+            PuckEnd(); //spawns puck eqach frame
+        }
+        
     }
 
     void PuckEnd()
     {
         if (transform.localScale.x <= 0.001f)
         {
-            if (goalie.transform.position.x >= puck.transform.position.x - 1.2f && goalie.transform.position.x <= puck.transform.position.x + 1.2f)// checking hitboxes for goalie
+            if (goalie.transform.position.x >= puck.transform.position.x - 1.2f && goalie.transform.position.x <= puck.transform.position.x + 1.2f)// checking x hitboxes for goalie
             {
-                Instantiate(puck); //makes duplicate puck
-                SAVE.Invoke();
+                if (goalie.transform.position.y >= puck.transform.position.y - 1 && goalie.transform.position.y <= puck.transform.position.y + 1)// checking y hitboxes for goalie
+                {
+                    Instantiate(puck); //makes duplicate puck
+                    SAVE.Invoke();
+                }
             }
             Destroy(gameObject); // Detroys puck when done
 
--- Assets/Scripts/Puck2.cs
+++ Assets/Scripts/Puck2.cs
@@ -18,12 +18,13 @@ public class Puck2 : MonoBehaviour
     public float YValue;
     public GameObject goalie;
     public GameObject puck;
+    public GameObject CrazyPuck;
     public GameObject score;
     public UnityEvent SAVE; // adding to the score when save the puck
+    public IEnumerator CRAZY;
     // Start is called before the first frame update
     void Start()
     {
-        //StartCoroutine(Save());
         XValue = Random.Range(minXPosition, maxXPosition); //random X value
         YValue = Random.Range(minYPosition, maxYPosition); //random Y value
 
@@ -32,6 +33,7 @@ public class Puck2 : MonoBehaviour
         SAVE = new UnityEvent();
         SAVE.AddListener(score.GetComponent<Score>().save);
 
+        CRAZY = Crazy();
     }
 
     // Update is called once per frame
@@ -40,20 +42,34 @@ public class Puck2 : MonoBehaviour
         t += Time.deltaTime; //makes sure the puck scales every frame evenly
         transform.localScale = Vector2.one * size.Evaluate(t); //scales down puck size
         transform.position = Vector3.Lerp(new Vector3(XValue - 1, YValue), new Vector3(XValue, YValue), crazy.Evaluate(t)); //Crazy puck movement
-        PuckEnd(); //spawns puck eqach frame
     }
+    public IEnumerator Crazy()
+    {
+        while (puck.GetComponent<Puck>().num == 10) 
+        {
+            if(CrazyPuck != null)
+            {
+                PuckEnd(); //spawns Crazy puck
+            }
+            yield return null;
+        }
+    }
+    
 
     void PuckEnd()
     {
         if (transform.localScale.x <= 0.001f)
         {
-            if (goalie.transform.position.x >= puck.transform.position.x - 1.2f && goalie.transform.position.x <= puck.transform.position.x + 1.2f)// checking hitboxes for goalie
+            if (goalie.transform.position.x >= CrazyPuck.transform.position.x - 1.2f && goalie.transform.position.x <= CrazyPuck.transform.position.x + 1.2f)// checking hitboxes for goalie
             {
-                Instantiate(puck); //makes duplicate puck
-                SAVE.Invoke();
+                if (goalie.transform.position.y >= CrazyPuck.transform.position.y - 1 && goalie.transform.position.y <= CrazyPuck.transform.position.y + 1)// checking y hitboxes for goalie
+                {
+                    Instantiate(CrazyPuck); //makes duplicate Crazy puck
+                    SAVE.Invoke();
+                }
             }
             Destroy(gameObject); // Detroys puck when done
-
+            CrazyPuck = null;
         }
     }
 }
